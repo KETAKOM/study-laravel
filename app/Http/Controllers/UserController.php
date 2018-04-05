61e6145b57c0fcefa2b660468dd36797e18dd912
shunki <ketakom2111178@gmail.com>
2018-04-05 06:04:46 +0000

    リポジトリをインターフェースを作成

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
-use App\Model\User;
+use App\Repositories\User\UserRepositoryInterface;
 
 class UserController extends Controller
 {
+    public function __construct(UserRepositoryInterface $user_repository) {
+      $this->user = $user_repository;
+    }
+    
     public function index() {
-        $users = User::all();
+        $users = $this->user->getFirstRecordByName('test');
         
         if (View::exists('user')) {
             return view('user')->with('users', $users);
